54ebc93b4692a7c7b5b565f4dbfe276919fe78c6
John_6670 <mayn2005m@gmail.com>
2024-07-25 20:39:42 +0330

    first setup

--- backend/projects/admin.py
+++ backend/projects/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 
-from .models import Project, Technology
+from .models import *
+
 
 # Register your models here.
-admin.site.register(Project)
-admin.site.register(Technology)
+class ProjectImageInline(admin.TabularInline):
+    model = ProjectImage
+    extra = 1
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'link']
+    inlines = [ProjectImageInline]
+    search_fields = ['title', 'description']
+    list_filter = ['technologies']
+
+
+class TechnologyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category']
+    search_fields = ['name']
+    list_filter = ['category']
+
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Technology, TechnologyAdmin)
+admin.site.register(ProjectImage)
---
+++ frontend/src/components/Home/Home.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Particle from "../Particle";
+import Summary from "./Summary";
+import Typewriter from "typewriter-effect";
+
+function Home() {
+    return (
+        <section>
+            <Container fluid className="home-section" id="home">
+                <Particle />
+                <Container className="home-content">
+                    <Row>
+                        <Col md={7} className="home-header">
+                            <h1 style={{ paddingBottom: 15 }} className="heading">
+                                Hi There!{" "}
+                                <span className="wave" role="img" aria-labelledby="wave">
+                                    👋🏻
+                                </span>
+                            </h1>
+
+                            <h1 className="heading-name">
+                                I'M
+                                <strong className="main-name"> SOUMYAJIT BEHERA</strong>
+                            </h1>
+
+                            <div style={{ padding: 50, textAlign: "left" }}>
+                                <Typewriter
+                                    options={{
+                                        strings: [
+                                            "Computer Engineering Student",
+                                        ],
+                                        autoStart: true,
+                                        loop: true,
+                                        deleteSpeed: 50,
+                                    }}
+                                />
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </Container>
+            <Summary />
+        </section>
+    );
+}
+
+export default Home;
