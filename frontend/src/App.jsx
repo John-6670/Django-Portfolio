e08d243df383afd4decb01bf27c8a3dcfd6a5763
John <john2005mm@gmail.com>
2024-07-27 17:09:57 +0330

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,19 +11,16 @@ import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
-import Resume from "./components/Resume/Resume";
 
 function App() {
     return (
         <Router>
             <div className="App">
                 <NavBar />
-                {/*<ScrollToTop />*/}
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/project" element={<Projects />} />
-                    <Route path="/resume" element={<Resume />} />
                     <Route path="*" element={<Navigate to="/"/>} />
                 </Routes>
                 <Footer />
