eb86d3b8c3247ccf177287a488f6bbe88e0ce269
John_6670 <mayn2005m@gmail.com>
2024-07-31 20:21:17 +0330

    no image

--- frontend/src/components/Projects/Projects.jsx
+++ frontend/src/components/Projects/Projects.jsx
@@ -1,51 +1,49 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import hotel from "../../Assets/Projects/Hotel.png";
 import drf from "../../Assets/Projects/DRF.jpg";
 import "../../Styles/Projects.css";
+import api from "../../api";
 
 function Projects() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    getProjects();
+  }, []);
+
+  const getProjects = () => {
+    api
+        .get("projects/")
+        .then((res) => res.data)
+        .then((data) => {
+          setProjects(data);
+          console.log(data);
+        })
+        .catch((err) => alert(err));
+  }
+
   return (
     <Container fluid className="project-section">
       <Container>
         <h1 className="project-heading">
-          My Recent <strong className="green">Works </strong>
+          My Recent <strong className="green">Works</strong>
         </h1>
         <p style={{ color: "white" }}>
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={drf}
-              title="Simple Social Media API"
-              description="This is a Django REST API project under development that provides functionalities for users to interact
-              with posts and comments. I'm working on improving the features and functionalities."
-              ghLink="https://github.com/John-6670/SimpleSocialApp"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={hotel}
-              title="Hotel Management System"
-              description="The Hotel Management System is a comprehensive software solution designed to streamline the operations of hotels.
-              This system facilitates the management of hotel activities including room booking, guest management, staff management, and report generation.
-              It is built using Java and Maven, ensuring a robust and scalable application. "
-              ghLink="https://github.com/John-6670/HotelManagementSystem"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={drf}
-              title="Note Taking API"
-              description="This project provides a user-friendly RESTful API for managing your notes.
-              Perfect for creating personal note-taking applications or integrating notes into other projects."
-              ghLink="https://github.com/John-6670/Django-RestApi"
-            />
-          </Col>
+          {projects.map((project) => (
+              <Col md={4} className="project-card" key={project.id}>
+                <ProjectCard
+                  imgPath={drf}
+                  title={project.title}
+                  description={project.description}
+                  ghLink={project.link}
+                  />
+              </Col>
+          ))}
         </Row>
       </Container>
     </Container>
